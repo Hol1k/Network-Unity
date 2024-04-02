5256f66d93faa84a7ddfe6528eed70e4ca2cebe8
Hol1k <danilugryumov96@gmail.com>
2024-04-02 14:52:25 +0500

    Remove ignored files & added WetherData loading by xml format

--- Assets/NetworkService.cs
+++ Assets/NetworkService.cs
@@ -1,8 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class NetworkService
 {
+    private const string xmlApi = "https://api.openweathermap.org/data/2.5/weather?q=Tyumen,ru&units=metric&mode=xml&appid=77ac539b7bec98276fbbc14fd06b0844";
+    
+    private IEnumerator CallAPI(string url, Action<string> callback)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            yield return request.SendWebRequest();
 
+            if (request.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.LogError($"network problem: {request.error}");
+            }
+            else if (request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError($"response error: {request.responseCode}");
+            }
+            else
+            {
+                callback(request.downloadHandler.text);
+            }
+        }
+    }
+
+    public IEnumerator GetWeatherXML(Action<string> callback)
+    {
+        return CallAPI(xmlApi, callback);
+    }
 }
--- Assets/Skyboxes/WeatherManager.cs
+++ Assets/Skyboxes/WeatherManager.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEngine;
 
 public class WeatherManager : MonoBehaviour, IGameManager
 {
     public ManagerStatus status {  get; private set; }
+
+    public float cloudValue { get; private set; }
 
     private NetworkService network;
 
@@ -13,6 +17,23 @@ public class WeatherManager : MonoBehaviour, IGameManager
         Debug.Log("Weather manager starting...");
 
         network = service;
+        StartCoroutine(network.GetWeatherXML(OnXmlDataLoaded));
+
+        status = ManagerStatus.Initializing;
+    }
+
+    public void OnXmlDataLoaded(string data)
+    {
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml(data);
+        XmlNode root = doc.DocumentElement;
+
+        XmlNode node = root.SelectSingleNode("clouds");
+        string value = node.Attributes["value"].Value;
+        cloudValue = Convert.ToInt32(value) / 100f;
+        Debug.Log($"Value: {cloudValue}");
+
+        Messenger.Broadcast(GameEvent.WEATHER_UPDATED);
 
         status = ManagerStatus.Started;
     }
