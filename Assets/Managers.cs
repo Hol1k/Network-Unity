3535f82b1487996f3bef091d10c7f230edbdff88
Hol1k <danilugryumov96@gmail.com>
2024-04-12 12:54:00 +0500

    added image downloading to banner

--- Assets/Managers.cs
+++ Assets/Managers.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(WeatherManager))]
+[RequireComponent(typeof(ImagesManager))]
 
 public class Managers : MonoBehaviour {
-	public static WeatherManager Weather { get; private set;}
+	public static WeatherManager Weather { get; private set; }
+    public static ImagesManager Images { get; private set; }
 
-	private List<IGameManager> startSequence;
+    private List<IGameManager> startSequence;
 	
 	void Awake() {
 		Weather = GetComponent<WeatherManager>();
+        Images = GetComponent<ImagesManager>();
 
-		startSequence = new List<IGameManager>();
+        startSequence = new List<IGameManager>();
 		startSequence.Add(Weather);
+        startSequence.Add(Images);
 
-		StartCoroutine(StartupManagers());
+        StartCoroutine(StartupManagers());
 	}
 
 	private IEnumerator StartupManagers() {
--- Assets/NetworkService.cs
+++ Assets/NetworkService.cs
@@ -7,7 +7,9 @@ using UnityEngine.Networking;
 public class NetworkService
 {
     private const string xmlApi = "https://api.openweathermap.org/data/2.5/weather?q=Tyumen,ru&units=metric&mode=xml&appid=77ac539b7bec98276fbbc14fd06b0844";
-    
+
+    private const string webImage = "https://unity-connect-prd.storage.googleapis.com/20200617/learn/images/207f76c5-b570-4efc-ac6e-b6fc15850008_prototype1.png";
+
     private IEnumerator CallAPI(string url, Action<string> callback)
     {
         using (UnityWebRequest request = UnityWebRequest.Get(url))
@@ -27,6 +29,13 @@ public class NetworkService
                 callback(request.downloadHandler.text);
             }
         }
+    }
+
+    public IEnumerator DownloadImage(Action<Texture2D> callback)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(webImage);
+        yield return request.SendWebRequest();
+        callback(DownloadHandlerTexture.GetContent(request));
     }
 
     public IEnumerator GetWeatherXML(Action<string> callback)
